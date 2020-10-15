708aba27126e2e7b922b2adde95a5b5830221642
qiuyi116 <qiuyi116@gmail.com>
2020-10-15 22:16:25 +0800

    update cp1

--- ch2/cp1.c
+++ ch2/cp1.c
@@ -6,18 +6,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <limits.h>
 
 #define BUFFERSIZE 4096
 #define COPYMODE   0644
 
 void oops(char *, char *);
 
-void main(int ac, char * av[]) {
+int main(int ac, char * av[]) {
     int in_fd, out_fd, n_chars;
     char buf[BUFFERSIZE];
+    char *in_path, *out_path;
 
     if (ac != 3) {
         fprintf(stderr, "usage: %s source destination\n", *av);
+        exit(1);
+    }
+
+    if ((in_path = realpath(av[1], NULL)) != NULL && (out_path = realpath(av[2], NULL)) != NULL) {
+        if (strlen(in_path) == strlen(out_path) && strncmp(in_path, out_path, strlen(in_path)) == 0) {
+            fprintf(stderr, "cp: '%s' and '%s' are the same file", av[1], av[2]);
+            exit(1);
+        }
+    } else {
+        perror("get realpath of file error.");
         exit(1);
     }
 
@@ -39,6 +52,7 @@ void main(int ac, char * av[]) {
     if (close(in_fd) == -1 || close(out_fd) == -1) {
         oops("Error closing files", "");
     }
+    return 0;
 }
 
 void oops(char* s1, char* s2) {
